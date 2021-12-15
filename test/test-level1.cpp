a7db1ea2622e69b2fc316b4faccb7a75ea79f598
keyehzy <msamuel@aluno.puc-rio.br>
2021-12-15 20:56:32 -0300

    use string-view plz

--- test/test-level1.cpp
+++ test/test-level1.cpp
@@ -3,85 +3,85 @@
 
 TEST(test, level1) {
   // Testing read of numbers
-  { EXPECT_EQ(print_quoted("1"), "1"); }
-  { EXPECT_EQ(print_quoted("7"), "7"); }
-  { EXPECT_EQ(print_quoted("  7   "), "7"); }
-  { EXPECT_EQ(print_quoted("-123"), "-123"); }
+  { EXPECT_EQ(print_quoted("1"_sv), "1"_sv); }
+  { EXPECT_EQ(print_quoted("7"_sv), "7"_sv); }
+  { EXPECT_EQ(print_quoted("  7   "_sv), "7"_sv); }
+  { EXPECT_EQ(print_quoted("-123"_sv), "-123"_sv); }
 
   // Testing read of symbols
-  { EXPECT_EQ(print_quoted("+"), "+"); }
-  { EXPECT_EQ(print_quoted("abc"), "abc"); }
-  { EXPECT_EQ(print_quoted("   abc   "), "abc"); }
-  { EXPECT_EQ(print_quoted("abc5"), "abc5"); }
-  { EXPECT_EQ(print_quoted("abc-def"), "abc-def"); }
+  { EXPECT_EQ(print_quoted("+"_sv), "+"_sv); }
+  { EXPECT_EQ(print_quoted("abc"_sv), "abc"_sv); }
+  { EXPECT_EQ(print_quoted("   abc   "_sv), "abc"_sv); }
+  { EXPECT_EQ(print_quoted("abc5"_sv), "abc5"_sv); }
+  { EXPECT_EQ(print_quoted("abc-def"_sv), "abc-def"_sv); }
 
   // Testing non-numbers starting with a dash.
-  { EXPECT_EQ(print_quoted("-"), "-"); }
-  { EXPECT_EQ(print_quoted("-abc"), "-abc"); }
-  { EXPECT_EQ(print_quoted("->>"), "->>"); }
+  { EXPECT_EQ(print_quoted("-"_sv), "-"_sv); }
+  { EXPECT_EQ(print_quoted("-abc"_sv), "-abc"_sv); }
+  { EXPECT_EQ(print_quoted("->>"_sv), "->>"_sv); }
 
   // Testing read of lists
-  { EXPECT_EQ(print_quoted("(+ 1 2)"), "(+ (1 2))"); }
-  { EXPECT_EQ(print_quoted("()"), "()"); }
-  { EXPECT_EQ(print_quoted("( )"), "()"); }
-  { EXPECT_EQ(print_quoted("(nil)"), "nil"); }
-  { EXPECT_EQ(print_quoted("((3 4))"), "(3 4)"); }
-  { EXPECT_EQ(print_quoted("(+ 1 (+ 2 3))"), "(+ (1 (+ (2 3))))"); }
+  { EXPECT_EQ(print_quoted("(+ 1 2)"_sv), "(+ (1 2))"_sv); }
+  { EXPECT_EQ(print_quoted("()"_sv), "()"_sv); }
+  { EXPECT_EQ(print_quoted("( )"_sv), "()"_sv); }
+  { EXPECT_EQ(print_quoted("(nil)"_sv), "nil"_sv); }
+  { EXPECT_EQ(print_quoted("((3 4))"_sv), "(3 4)"_sv); }
+  { EXPECT_EQ(print_quoted("(+ 1 (+ 2 3))"_sv), "(+ (1 (+ (2 3))))"_sv); }
   {
-    EXPECT_EQ(print_quoted("  ( +   1   (+   2 3   )   )  "),
-              "(+ (1 (+ (2 3))))");
+    EXPECT_EQ(print_quoted("  ( +   1   (+   2 3   )   )  "_sv),
+              "(+ (1 (+ (2 3))))"_sv);
   }
 
-  { EXPECT_EQ(print_quoted("(* 1 2)"), "(* (1 2))"); }
-  { EXPECT_EQ(print_quoted("(** 1 2)"), "(** (1 2))"); }
-  { EXPECT_EQ(print_quoted("(* -3 6)"), "(* (-3 6))"); }
-  { EXPECT_EQ(print_quoted("(()())"), "(() ())"); }
+  { EXPECT_EQ(print_quoted("(* 1 2)"_sv), "(* (1 2))"_sv); }
+  { EXPECT_EQ(print_quoted("(** 1 2)"_sv), "(** (1 2))"_sv); }
+  { EXPECT_EQ(print_quoted("(* -3 6)"_sv), "(* (-3 6))"_sv); }
+  { EXPECT_EQ(print_quoted("(()())"_sv), "(() ())"_sv); }
 
   // Testing read of nil/true/false
-  { EXPECT_EQ(print_quoted("nil"), "nil"); }
-  { EXPECT_EQ(print_quoted("true"), "true"); }
-  { EXPECT_EQ(print_quoted("false"), "false"); }
+  { EXPECT_EQ(print_quoted("nil"_sv), "nil"_sv); }
+  { EXPECT_EQ(print_quoted("true"_sv), "true"_sv); }
+  { EXPECT_EQ(print_quoted("false"_sv), "false"_sv); }
 
   // Testing read of strings
-  { EXPECT_EQ(print_quoted(R"("abc")"), R"("abc")"); }
-  { EXPECT_EQ(print_quoted(R"(   "abc"   )"), R"("abc")"); }
+  { EXPECT_EQ(print_quoted(R"---("abc")---"), R"---("abc")---"); }
+  { EXPECT_EQ(print_quoted(R"---(   "abc"   )---"), R"---("abc")---"); }
   {
-    EXPECT_EQ(print_quoted(R"STR("abc (with parens)")STR"),
-              R"STR("abc (with parens)")STR");
+    EXPECT_EQ(print_quoted(R"---("abc (with parens)")---"),
+              R"---("abc (with parens)")---");
   }
-  { EXPECT_EQ(print_quoted(R"("abc\"def")"), R"("abc\"def")"); }
-  { EXPECT_EQ(print_quoted(R"("")"), R"("")"); }
-  { EXPECT_EQ(print_quoted(R"("\\")"), R"("\\")"); }
+  { EXPECT_EQ(print_quoted(R"---("abc\"def")---"), R"---("abc\"def")---"); }
+  { EXPECT_EQ(print_quoted(R"---("")---"), R"---("")---"); }
+  { EXPECT_EQ(print_quoted(R"---("\\")---"), R"---("\\")---"); }
   {
-    EXPECT_EQ(print_quoted(R"("\\\\\\\\\\\\\\\\\\")"),
-              R"("\\\\\\\\\\\\\\\\\\")");
+    EXPECT_EQ(print_quoted(R"---("\\\\\\\\\\\\\\\\\\")---"),
+              R"---("\\\\\\\\\\\\\\\\\\")---");
   }
 
-  { EXPECT_EQ(print_quoted(R"("&")"), R"("&")"); }
-  { EXPECT_EQ(print_quoted(R"("'")"), R"("'")"); }
-  { EXPECT_EQ(print_quoted(R"("(")"), R"("(")"); }
-  { EXPECT_EQ(print_quoted(R"RAW_STR(")")RAW_STR"), R"RAW_STR(")")RAW_STR"); }
-  { EXPECT_EQ(print_quoted(R"("*")"), R"("*")"); }
-  { EXPECT_EQ(print_quoted(R"("+")"), R"("+")"); }
-  { EXPECT_EQ(print_quoted(R"(",")"), R"(",")"); }
-  { EXPECT_EQ(print_quoted(R"("-")"), R"("-")"); }
-  { EXPECT_EQ(print_quoted(R"("/")"), R"("/")"); }
-  { EXPECT_EQ(print_quoted(R"(":")"), R"(":")"); }
-  { EXPECT_EQ(print_quoted(R"(";")"), R"(";")"); }
-  { EXPECT_EQ(print_quoted(R"("<")"), R"("<")"); }
-  { EXPECT_EQ(print_quoted(R"("=")"), R"("=")"); }
-  { EXPECT_EQ(print_quoted(R"(">")"), R"(">")"); }
-  { EXPECT_EQ(print_quoted(R"("?")"), R"("?")"); }
-  { EXPECT_EQ(print_quoted(R"("@")"), R"("@")"); }
-  { EXPECT_EQ(print_quoted(R"("[")"), R"("[")"); }
-  { EXPECT_EQ(print_quoted(R"("]")"), R"("]")"); }
-  { EXPECT_EQ(print_quoted(R"("^")"), R"("^")"); }
-  { EXPECT_EQ(print_quoted(R"("_")"), R"("_")"); }
-  { EXPECT_EQ(print_quoted(R"("`")"), R"("`")"); }
-  { EXPECT_EQ(print_quoted(R"("{")"), R"("{")"); }
-  { EXPECT_EQ(print_quoted(R"("}")"), R"("}")"); }
-  { EXPECT_EQ(print_quoted(R"("~")"), R"("~")"); }
-  { EXPECT_EQ(print_quoted(R"("!")"), R"("!")"); }
+  { EXPECT_EQ(print_quoted(R"---("&")---"), R"---("&")---"); }
+  { EXPECT_EQ(print_quoted(R"---("'")---"), R"---("'")---"); }
+  { EXPECT_EQ(print_quoted(R"---("(")---"), R"---("(")---"); }
+  { EXPECT_EQ(print_quoted(R"---(")")---"), R"---(")")---"); }
+  { EXPECT_EQ(print_quoted(R"---("*")---"), R"---("*")---"); }
+  { EXPECT_EQ(print_quoted(R"---("+")---"), R"---("+")---"); }
+  { EXPECT_EQ(print_quoted(R"---(",")---"), R"---(",")---"); }
+  { EXPECT_EQ(print_quoted(R"---("-")---"), R"---("-")---"); }
+  { EXPECT_EQ(print_quoted(R"---("/")---"), R"---("/")---"); }
+  { EXPECT_EQ(print_quoted(R"---(":")---"), R"---(":")---"); }
+  { EXPECT_EQ(print_quoted(R"---(";")---"), R"---(";")---"); }
+  { EXPECT_EQ(print_quoted(R"---("<")---"), R"---("<")---"); }
+  { EXPECT_EQ(print_quoted(R"---("=")---"), R"---("=")---"); }
+  { EXPECT_EQ(print_quoted(R"---(">")---"), R"---(">")---"); }
+  { EXPECT_EQ(print_quoted(R"---("?")---"), R"---("?")---"); }
+  { EXPECT_EQ(print_quoted(R"---("@")---"), R"---("@")---"); }
+  { EXPECT_EQ(print_quoted(R"---("[")---"), R"---("[")---"); }
+  { EXPECT_EQ(print_quoted(R"---("]")---"), R"---("]")---"); }
+  { EXPECT_EQ(print_quoted(R"---("^")---"), R"---("^")---"); }
+  { EXPECT_EQ(print_quoted(R"---("_")---"), R"---("_")---"); }
+  { EXPECT_EQ(print_quoted(R"---("`")---"), R"---("`")---"); }
+  { EXPECT_EQ(print_quoted(R"---("{")---"), R"---("{")---"); }
+  { EXPECT_EQ(print_quoted(R"---("}")---"), R"---("}")---"); }
+  { EXPECT_EQ(print_quoted(R"---("~")---"), R"---("~")---"); }
+  { EXPECT_EQ(print_quoted(R"---("!")---"), R"---("!")---"); }
 
   // Testing read of quoting
   // { EXPECT_EQ(print_quoted(R"('1)"), R"((quote (1 nil)))"); }
--- test/test-level2.cpp
+++ test/test-level2.cpp
@@ -3,37 +3,40 @@
 
 TEST(test, level2) {
   // Testing quote of lists
-  { EXPECT_EQ(print("'(+ 1 2)"), "(+ (1 2))"); }
-  { EXPECT_EQ(print("'()"), "()"); }
-  { EXPECT_EQ(print("'( )"), "()"); }
-  { EXPECT_EQ(print("'(nil)"), "nil"); }
-  { EXPECT_EQ(print("'((3 4))"), "(3 4)"); }
-  { EXPECT_EQ(print("'(+ 1 (+ 2 3))"), "(+ (1 (+ (2 3))))"); }
-  { EXPECT_EQ(print("'  ( +   1   (+   2 3   )   )  "), "(+ (1 (+ (2 3))))"); }
+  { EXPECT_EQ(print("'(+ 1 2)"_sv), "(+ (1 2))"_sv); }
+  { EXPECT_EQ(print("'()"_sv), "()"_sv); }
+  { EXPECT_EQ(print("'( )"_sv), "()"_sv); }
+  { EXPECT_EQ(print("'(nil)"_sv), "nil"_sv); }
+  { EXPECT_EQ(print("'((3 4))"_sv), "(3 4)"_sv); }
+  { EXPECT_EQ(print("'(+ 1 (+ 2 3))"_sv), "(+ (1 (+ (2 3))))"_sv); }
+  {
+    EXPECT_EQ(print("'  ( +   1   (+   2 3   )   )  "_sv),
+              "(+ (1 (+ (2 3))))"_sv);
+  }
 
   // Testing if
-  { EXPECT_EQ(print("(if #t 2 3)"), "2"); }
-  { EXPECT_EQ(print("(if #f 2 3)"), "3"); }
+  { EXPECT_EQ(print("(if #t 2 3)"_sv), "2"_sv); }
+  { EXPECT_EQ(print("(if #f 2 3)"_sv), "3"_sv); }
 
   // Testing begin
-  { EXPECT_EQ(print("(begin 1 2 3)"), "3"); }
-  { EXPECT_EQ(print("(begin 1 2)"), "2"); }
-  { EXPECT_EQ(print("(begin 1)"), "1"); }
+  { EXPECT_EQ(print("(begin 1 2 3)"_sv), "3"_sv); }
+  { EXPECT_EQ(print("(begin 1 2)"_sv), "2"_sv); }
+  { EXPECT_EQ(print("(begin 1)"_sv), "1"_sv); }
 
   // Testing set!
-  { EXPECT_EQ(print("(begin (set! x 42) x)"), "42"); }
-  { EXPECT_EQ(print("(begin (set! x 42) y)"), "y"); }  // autoquote
+  { EXPECT_EQ(print("(begin (set! x 42) x)"_sv), "42"_sv); }
+  { EXPECT_EQ(print("(begin (set! x 42) y)"_sv), "y"_sv); }  // autoquote
 
   // Testing binary operation
-  { EXPECT_EQ(print("(+ 1 2)"), "3"); }
-  { EXPECT_EQ(print("(- 1 2)"), "-1"); }
-  { EXPECT_EQ(print("(* 2 3)"), "6"); }
-  { EXPECT_EQ(print("(/ 6 2)"), "3"); }
-  { EXPECT_EQ(print("(% 5 3)"), "2"); }
+  { EXPECT_EQ(print("(+ 1 2)"_sv), "3"_sv); }
+  { EXPECT_EQ(print("(- 1 2)"_sv), "-1"_sv); }
+  { EXPECT_EQ(print("(* 2 3)"_sv), "6"_sv); }
+  { EXPECT_EQ(print("(/ 6 2)"_sv), "3"_sv); }
+  { EXPECT_EQ(print("(% 5 3)"_sv), "2"_sv); }
 
-  { EXPECT_EQ(print("(+ 1.0 0.5)"), "1.500000"); }
-  { EXPECT_EQ(print("(- 1.5 2.5)"), "-1.000000"); }
-  { EXPECT_EQ(print("(* 2.5 2)"), "5.000000"); }
-  { EXPECT_EQ(print("(/ 6.0 1.5)"), "4.000000"); }
-  { EXPECT_EQ(print("(% 1.5 1.0)"), "0.500000"); }
+  { EXPECT_EQ(print("(+ 1.0 0.5)"_sv), "1.500000"_sv); }
+  { EXPECT_EQ(print("(- 1.5 2.5)"_sv), "-1.000000"_sv); }
+  { EXPECT_EQ(print("(* 2.5 2)"_sv), "5.000000"_sv); }
+  { EXPECT_EQ(print("(/ 6.0 1.5)"_sv), "4.000000"_sv); }
+  { EXPECT_EQ(print("(% 1.5 1.0)"_sv), "0.500000"_sv); }
 }
