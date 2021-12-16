dbb48fb4e8cbfb4e9d59bf35689ce2c24f44e9ab
keyehzy <msamuel@aluno.puc-rio.br>
2021-12-16 16:31:49 -0300

    Repl

--- src/esquema.cpp
+++ src/esquema.cpp
@@ -1,1 +1,15 @@
-int main(int argc, char **argv) { return 0; }
+#include "esquema/print.h"
+#include "esquema/string_view.h"
+#include <editline/readline.h>
+#include <iostream>
+
+int main(void) {
+  char *buffer;
+  while ((buffer = readline("esquema> ")) != NULL) {
+    string res = print(buffer);
+    if (!res.empty()) {
+      std::cout << res << std::endl;
+    }
+  }
+  return 0;
+}
