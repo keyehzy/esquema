d0a6a770dce82a2c652eb976f839b14c4cb9d8f0
keyehzy <msamuel@aluno.puc-rio.br>
2021-12-30 09:11:34 -0300

    Make use of cond and va_args in our ESQUEMA_ERROR macro

--- src/esquema/assert.h
+++ src/esquema/assert.h
@@ -1,16 +1,15 @@
 #include <cstdlib>
 
-#define ESQUEMA_ASSERT(...)                                       \
+#define ESQUEMA_ASSERT(cond, ...)                                 \
   do {                                                            \
-    if (__VA_ARGS__) {                                            \
-    } else {                                                      \
+    if (!(cond)) {                                                \
       report_failure(__FILE__, __LINE__, __func__, #__VA_ARGS__); \
       __builtin_trap();                                           \
     }                                                             \
   } while (false)
 
 #define ESQUEMA_NOT_REACHED() ESQUEMA_ASSERT(false)
-#define ESQUEMA_ERROR(...) ESQUEMA_ASSERT(false && __VA_ARGS__)
+#define ESQUEMA_ERROR(...) ESQUEMA_ASSERT(false, __VA_ARGS__)
 
 void report_failure(const char* filename, int line, const char* function_name,
                     const char* message);
